0a3f928ce9fb0661be128add6fa57049b6a7db45
Abhiroop007 <abhirooptandon1@abhiroop007s-mac-mini.local>
2015-11-22 18:12:07 +0000

    Play button working, scene transition fixed

--- SpaceRun/MenuScene.swift
+++ SpaceRun/MenuScene.swift
@@ -19,22 +19,18 @@ class MenuScene: SKScene {
         playButton = SKSpriteNode(texture: playButtonTex)
         playButton.position = CGPointMake(frame.midX, frame.midY)
         self.addChild(playButton)
-        
-        
-        func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-            
-            
-            if let touch = touches.first as? UITouch {
-                let pos = touch.locationInNode(self)
-                let node = self.nodeAtPoint(pos)
-                
-                if node == playButton {
-                    //if let sview = view {
-                        let scene = GameScene(fileNamed:"GameScene") as GameScene?
-                        scene!.scaleMode = SKSceneScaleMode.AspectFill
-                        view.presentScene(scene)
-                    //}
-                }   
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesBegan(touches, withEvent: event)
+            if let location = touches.first?.locationInNode(self){
+                let touchedNode = nodeAtPoint(location)
+    
+                if touchedNode == playButton{
+                    print("touch")
+                    let transition = SKTransition.revealWithDirection(.Down, duration: 1.0)
+                    let nextScene = GameScene(size:scene!.size)
+                    nextScene.scaleMode = .AspectFill
+                    scene?.view?.presentScene(nextScene, transition: transition)
             }
         }
     }
